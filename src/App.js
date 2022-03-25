407c8ede53682d8bed9c43128af847d8db50b535
Candy Fair <candice.bastien@gmail.com>
2022-03-25 11:18:48 +0100

    First commit - desktop version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Clock from './components/Clock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './styles/reset.css';
+import './styles/styles.scss'
+
+class App extends React.Component {
+  state = {
+    deadline: 'September 16, 2022',
+    newDeadline: '',
+  }
+
+  changeDeadline() {
+    this.setState({
+      deadline: this.state.newDeadline,
+    })
+  }
+  
+  render() {
+    const { deadline } = this.state;
+
+    return (
+
+      <div className="container">
+        <div className="title">Deadline counter</div>
+        <h1>Number of days until {deadline}</h1>
+        <Clock deadline={this.state.deadline} />
+        <input 
+          placeholder="Enter a new date"
+          type="text"
+          className="input"
+          onChange={(evt) => {
+            this.setState({newDeadline: evt.target.value})
+          }}
+        />
+        <button 
+          className="button"
+          onClick={() => this.changeDeadline()}
+        >Submit</button>
+      </div>
+
+    );
+
+  } 
+  
+
 }
 
 export default App;
